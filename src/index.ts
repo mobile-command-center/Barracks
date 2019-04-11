e89220d012596c61c7e27a578e2cc89186a48e8c
Luyin <myraous@gmail.com>
2019-04-11 22:59:57 +0900

    chore: set multipart formdata retriving

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as multer from 'multer';
 
 const app = express();
+const upload = multer();
 
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/api', function(req, res) {
-  res.send('Hellow World2');
+app.post('/command-center/v1/register', upload.none(), function(req, res) {
+  console.log(req.body);
+  res.send('가입 신청서가 성공적으로 전송되었습니다.');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
 });
