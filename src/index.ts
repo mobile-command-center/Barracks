26783e13eae62c3e1436d39777b3b54d88fb8603
Luyin <myraous@gmail.com>
2019-04-13 22:52:18 +0900

    feat: read package.json

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
 import * as multer from 'multer';
+import * as fs from 'fs';
+import RegisterService from './services/RegisterService';
 
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 const app = express();
 const upload = multer();
 
@@ -13,7 +16,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/command-center/v1/register', upload.none(), function(req, res) {
+  const registerService = RegisterService.getInstance(pkg.mailInfo);
+
   console.log(req.body);
+  registerService.sendMail({
+    from: pkg.mailInfo.from,
+    to: pkg.mailInfo.to,
+    subject: `${req.body.c_name}가입 신청서`,
+    text: '가입 신청서 샘플이요'
+  });
   res.send('가입 신청서가 성공적으로 전송되었습니다.');
 })
 
