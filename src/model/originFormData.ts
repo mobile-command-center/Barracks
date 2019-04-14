fb0b1b811463172c2a96fe21315190f9e1588135
Luyin <myraous@gmail.com>
2019-04-14 15:03:18 +0900

    feat: add RegisterDTO

---
+++ src/model/originFormData.ts
@@ -0,0 +1,45 @@
+export default interface originFormData {
+    vendor: string; //LG
+    telephone_carrier_move_chk: string //true/false
+    c_name: string;// 김진구
+    c_tel2_type: string; //LG알뜰폰
+    c_tel21: string; //010
+    c_tel22: string; //9361
+    c_tel23: string; //6635
+    g_auth: string; //본인
+    c_tel11: string; //070
+    c_tel12: string; //4123
+    c_tel13: string; //6635
+    c_email1: string; //myraous
+    c_email2: string; //hanmail.net
+    emailCheck: string; //etc
+    c_zipcode1: string; //463
+    c_zipcode2: string; //894
+    c_address: string; //경기 성남시 분당구 동판교로 226 (삼평동, 봇들마을4단지아파트) 411동 104호
+    c_jumin1: string; //880512
+    g_payment_method: string; //은행
+    g_bank_cd: string; //4
+    g_bank_no: string; //620-209007-853
+    g_bank_holder: string; //김진구
+    g_card_cd:string; //3
+    g_card_no:string; //카드 번호
+    g_card_gigan:string; //카드 유효기간 년
+    g_card_gigan1: string; //카드 유효기간 월
+    g_card_holder: string; //카드 주
+    content_copy: string; //필요없는 값, 위와 같은 여부
+    g_sp_bank_code: string; //3
+    g_sp_bank_acount: string; //620-2023307-853
+    g_sp_bank_holder: string; //김진구
+    board_internet: string; //와이파이기본_광랜안심(100M)
+    board_tv: string; //TV베이직 - 183채널
+    board_tel: string; //WiFi(무선)전화 - 번호이동Y
+    board_settop: string; //UHD셋탑
+    board_wifi: string; //신청
+    g_code_promise: string; //3년약정
+    g_move_company: string; //LG
+    g_move_tel1: string; //123123
+    g_move_auth: string; // 지로납부 뒤 네자리
+    g_move_no: string; //2323
+    g_bigo: string; //비고란이다 ㅎㅎㅎ
+    w_agree: string; //true
+}
